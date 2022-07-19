5ff8919e71efd77b35a7dbf45ffb7989fa2c24b8
davidta123_js <david@studio31.io>
2022-07-18 22:59:17 -0600

    Remove wheel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,7 @@ import StyleBackground from '../styleComponents/StyleBackground'
 import BG from '../images/bg-project.jpg'
 import { useStaticQuery, graphql, Link } from "gatsby"
 import MyImage from '../hooks/LazyLoad'
+import { isBrowser } from '../globlas/window'
 
 const Projects = () => {
   const data = useStaticQuery(graphql`
@@ -38,22 +39,29 @@ const Projects = () => {
 
   const scrollContainer = useRef(null);
     useEffect(() => {
-      scrollContainer.current.addEventListener("wheel", (evt) => {
-        evt.preventDefault();
-        scrollContainer.current.scrollLeft += evt.deltaY/1.5;
-      });
-     
-      
+      if (isBrowser) {
+        window.addEventListener("resize", () =>{
+          if (window.innerWidth > 1200) {
+            scrollContainer.current.addEventListener("wheel", (evt) => {
+              evt.preventDefault();
+              scrollContainer.current.scrollLeft += evt.deltaY/1.5;
+            });     
+          }else{
+            scrollContainer.current.removeEventListener("wheel", () =>{
+              console.log('remove');
+            })
+          }
+      }); 
+      }
     }, [])
     const allProjects = data.allPrismicProjects.edges;
   return (
    <div id="projects">
-     <StyleBackground bg_image={BG}>
+     <StyleBackground bg_image={BG} className="bg-projects">
       <div className="contianer-scroll main" ref={scrollContainer}>
       {
             allProjects.map((project, index) =>{
               const {title_project, description_ptoject, link_project, link_code, image_project } = project.node.data;
-             console.log(image_project);
               return (
                 <div className="wrapper" key={index}> 
                   <div className="card">
@@ -61,11 +69,10 @@ const Projects = () => {
                       <div className="info">
                           <h1>{title_project.text}</h1>
                           <p>{description_ptoject.text}</p>
-                          <Link to={link_project.url}>View Project</Link>
+                          <Link to={link_project.url} target="_blank">View Project</Link>
                       </div>
                   </div>
-                </div>
-                
+                </div>             
               )
             }
             )
