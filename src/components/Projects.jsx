f9a5c2aa4743b28065798ed7292d4363faf28be8
David <da3909622@gmail.com>
2026-05-08 13:29:38 -0600

    refactor: migrate project cards to responsive CSS grid layout and simplify component structure

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,3 @@
-import { useRef } from 'react'
 import { usePrismicData } from '../prismic/hooks'
 import StyleBackground from '../styleComponents/StyleBackground'
 import BG from '../images/bg-project.jpg'
@@ -8,32 +7,30 @@ const Projects = () => {
   const { data } = usePrismicData('projects')
   const allProjects = data || []
 
-  const renderCard = () =>
-    allProjects.map((project, index) => {
-      const { title_project, description_ptoject, link_project, link_code, image_project } = project.data || {}
-      return (
-        <div className="wrapper" key={index}>
-          <div className="card">
-            <MyImage image={image_project?.url} />
-            <div className="info">
-              <h1>{title_project?.[0]?.text}</h1>
-              <p>{description_ptoject?.[0]?.text}</p>
-              <a href={link_project?.url} target="_blank" rel="noreferrer">
-                View Project
-              </a>
-            </div>
-          </div>
-        </div>
-      )
-    })
-
   return (
     <div id="projects">
       <StyleBackground $bg_image={BG} className="bg-projects">
-        <div className="contianer-scroll main ld-visible">
-          {renderCard()}
+        <div className="container mx-auto px-4 py-12">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
+            {allProjects.map((project, index) => {
+              const { title_project, description_ptoject, link_project, image_project } = project.data || {}
+              return (
+                <div className="card" key={index}>
+                  <MyImage image={image_project?.url} />
+                  <div className="info">
+                    <h1>{title_project?.[0]?.text}</h1>
+                    <p>{description_ptoject?.[0]?.text}</p>
+                    {link_project?.url && (
+                      <a href={link_project.url} target="_blank" rel="noreferrer">
+                        View Project
+                      </a>
+                    )}
+                  </div>
+                </div>
+              )
+            })}
+          </div>
         </div>
-        <div className="contianer-scroll main ld-hide">{renderCard()}</div>
       </StyleBackground>
     </div>
   )
